6b1ba469aa748e341c7ded0ca6e4883bc2c4bc85
Tseard <ploeggamer72@gmail.com>
2025-11-26 16:40:11 +0100

    Changed the Volume Function

--- Functions/CylinderVolume.m
+++ Functions/CylinderVolume.m
@@ -1,4 +1,4 @@
-function [V1,V2] = CylinderVolume(Ca,Cyl)
+function V = CylinderVolume(Ca,Cyl)
 % This function provides the cylinder volume as function of 
 % Ca : Crankangle [degrees]
 % Cyl :  a struct containing
@@ -8,10 +8,7 @@ function [V1,V2] = CylinderVolume(Ca,Cyl)
 %   Cyl.CompressionRatio    : Compession Ratio
 %   Cyl.TDCangle            : Angle associated with the Top Dead Center
 %----------------------------------------------------------------------
-fprintf('WARNING------------------------------------------------------------------\n');
-fprintf(' Function have been modified\n');
-fprintf(' This function is %s\n',mfilename('fullpath'));
-fprintf('END OF WARNING ----------------------------------------------------------\n');
+
 B   = Cyl.Bore;
 S   = Cyl.Stroke;
 cr  = Cyl.CompressionRatio;
@@ -21,7 +18,9 @@ l   = Cyl.ConRod;
 CAl     = Ca-Cyl.TDCangle;
 Vd      = pi*(B/2)^2*S;
 Vc      = Vd/(cr-1);
-V1      = Vc + Vd*(sind(CAl+90)+1)/2; % 'sind' is the sine function taking arguments in degrees instead of radians
-V2      = Vc + Vd*( modified_cosine(CAl,Cyl) );
+% The next line calculates at which position the piston is as a value
+% between 0 and 1, with 0 being top position and 1 bottom position.
+x = (cosd(CAl) - (sqrt(l^2 - (r*sind(CAl)).^2) - l)/r +1)/2;
+V      = Vc + Vd*x;
 
 
--- Simple.m
+++ Simple.m
@@ -57,39 +57,19 @@ line([CaIVC CaIVC],YLIM,'LineWidth',1,'Color','b'); % Plot a vertical line at IV
 line([CaEVO CaEVO],YLIM,'LineWidth',1,'Color','r'); % Plot a vertical line at EVO. Just for reference not a particular reason.
 set(gca,'XTick',[-360:60:360],'XGrid','on','YGrid','on');        % I like specific axis labels. Matter of taste
 title('All cycles in one plot.')
-%% pV-diagram Compare Cos with Modified Cos
-[V1,V2] = CylinderVolume(Ca(:,iselect),Cyl);
-f2 = figure(2);
-subplot(1,2,1)
-hold on
-plot(V1/dm^3,p(:,iselect)/bara,"Color","r");
-plot(V2/dm^3,p(:,iselect)/bara,"Color","g");
-hold off
-legend(["Cosine","Modified Cosine"])
-xlabel('V [dm^3]');ylabel('p [bar]');               % Always add axis labels
-xlim([0 0.8]);ylim([0.5 50]);                      % Matter of taste
-set(gca,'XTick',[0:0.1:0.8],'XGrid','on','YGrid','on');        % I like specific axis labels. Matter of taste
-title({'pV-diagram'})
-subplot(1,2,2)
-loglog(V1/dm^3,p(:,iselect)/bara,"r",V2/dm^3,p(:,iselect)/bara,"g");
-legend(["Cosine","Modified Cosine"])
-xlabel('V [dm^3]');ylabel('p [bar]');               % Always add axis labels
-xlim([0.02 0.8]);ylim([0 50]);                      % Matter of taste
-set(gca,'XTick',[0.02 0.05 0.1 0.2 0.5 0.8],...
-    'YTick',[0.5 1 2 5 10 20 50],'XGrid','on','YGrid','on');        % I like specific axis labels. Matter of taste
-title({'pV-diagram'})
 
 %% Results
+V = CylinderVolume(Ca(:,iselect),Cyl);
 
-f3 = figure(3);
+f2 = figure(2);
 subplot(1,2,1)
-plot(V2/dm^3,p(:,iselect)/bara,"Color","#00FFFF");
+plot(V/dm^3,p(:,iselect)/bara,"Color","#00FFFF");
 xlabel('V [dm^3]');ylabel('p [bar]');               % Always add axis labels
 xlim([0 0.8]);ylim([0.5 50]);                      % Matter of taste
 set(gca,'XTick',[0:0.1:0.8],'XGrid','on','YGrid','on');        % I like specific axis labels. Matter of taste
 title({'pV-diagram'})
 subplot(1,2,2)
-loglog(V2/dm^3,p(:,iselect)/bara,"Color","#00FFFF");
+loglog(V/dm^3,p(:,iselect)/bara,"Color","#00FFFF");
 xlabel('V [dm^3]');ylabel('p [bar]');               % Always add axis labels
 xlim([0.02 0.8]);ylim([0 50]);                      % Matter of taste
 set(gca,'XTick',[0.02 0.05 0.1 0.2 0.5 0.8],...
