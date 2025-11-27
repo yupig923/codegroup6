fcaa6b57b29b30b38f7ab01b74a878b2a641e607
Tseard <ploeggamer72@gmail.com>
2025-11-27 15:21:04 +0100

    Added a template

---
+++ Template.m
@@ -0,0 +1,20 @@
+% This template is used as a clean start for if anyone wants to make a code
+% for a specific purpose (like plotting graphs). All the Important data is
+% already put in here.
+
+clear all; clc;close all;
+% Units
+mm      = 1e-3;dm=0.1;
+bara    = 1e5;
+MJ      = 1e6;
+kWhr    = 1000*3600;
+volperc = 0.01; % Emissions are in volume percentages
+ppm     = 1e-6; % Some are in ppm (also a volume- not a mass-fraction)
+g       = 1e-3;
+s       = 1;
+% Engine geom data (check if these are correct)
+Cyl.Bore                = 104*mm;
+Cyl.Stroke              = 85*mm;
+Cyl.CompressionRatio    = 21.5;
+Cyl.ConRod              = 136.5*mm;
+Cyl.TDCangle            = 180;
