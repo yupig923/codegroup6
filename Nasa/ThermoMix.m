e91e2a51ef8bb638b40c782c72b5c3a1e29ebfee
Tseard <ploeggamer72@gmail.com>
2025-12-14 12:00:44 +0100

    Gamma Function works better now,
    I have changed the Thermomix.m to also accept matrixes. the calculation has not changed

--- Nasa/ThermoMix.m
+++ Nasa/ThermoMix.m
@@ -1,17 +1,26 @@
 function [Cp,Cv,H,E] = ThermoMix(Y,T,Sp)
 global Runiv
-%
-% Y = squeeze(Yf);
-Ma=0;
-for i=1:length(Sp)
-    Hi(i) = HNasa(T,Sp(i));
-    Cpi(i)= CpNasa(T,Sp(i));
-    Ma    = Ma+Y(i)/Sp(i).Mass;
-end
-Ma = 1/Ma;
-H  = Y*Hi';  
-E  = H-Runiv/Ma*T;
-Cp = Y*Cpi';
-Cv = Cp- Runiv/Ma;
+
+Tsize = size(T);
+T = T(:).';                % flatten
+nT = numel(T);
+nSp = length(Sp);
+
+Hi  = zeros(nSp, nT);
+Cpi = zeros(nSp, nT);
+
+Ma_inv = 0;
+for i = 1:nSp
+    Hi(i,:)  = HNasa(T, Sp(i));
+    Cpi(i,:) = CpNasa(T, Sp(i));
+    Ma_inv   = Ma_inv + Y(i)/Sp(i).Mass;
 end
 
+Ma = 1/Ma_inv;
+
+H  = reshape(Y * Hi,  Tsize);
+Cp = reshape(Y * Cpi, Tsize);
+
+E  = H - Runiv/Ma .* reshape(T, Tsize);
+Cv = Cp - Runiv/Ma;
+end
